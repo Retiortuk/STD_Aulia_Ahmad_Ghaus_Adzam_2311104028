e578c2b206128e85f8bc466f9a86e5f32d3b5825
Retiortuk <aauliagazzam@gmail.com>
2024-09-26 21:51:04 +0700

    feat: operasi_aritmatika

---
+++ 01_Pengenalan_CPP_Bagian_1/operasi_aritmatika.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int bil1 = 5, bil2 = 10, hasil1 ;
+    float bil3 = 3.5, bil4 = 12.2, hasil2 ;
+
+    // Int
+    hasil1 = bil1 + bil2;
+    cout << hasil1 << endl ;
+    hasil1 = bil1 * bil2;
+    cout << hasil1 << endl ;
+    hasil1 = bil1 - bil2;
+    cout << hasil1 << endl ;
+    hasil1 = bil1 / bil2;
+    cout << hasil1 << endl ;
+    hasil1 = bil1 % bil2;
+    cout << hasil1 << endl ;
+    // Modulo
+    hasil2 = bil3 / bil4;
+    cout << hasil2 << endl ;
+
+    return 0;
+}
