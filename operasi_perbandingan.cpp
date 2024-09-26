e0fbab5f9850acd5ed840106c5d1b869581b4ab9
Retiortuk <aauliagazzam@gmail.com>
2024-09-26 22:06:13 +0700

    feat: operasi perbandingan

---
+++ operasi_perbandingan.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int bil1 = 15, bil2 = 30, hasil ;
+
+    hasil = bil1 > bil2 ;
+    cout << hasil << endl ;
+    hasil = bil1 >= bil2 ;
+    cout << hasil << endl ;
+    hasil = bil1 < bil2 ;
+    cout << hasil << endl ;
+    hasil = bil1 <= bil2 ;
+    cout << hasil << endl ;
+    hasil = bil1 == bil2 ;
+    cout << hasil << endl ;
+    hasil = bil1 != bil2 ;
+    cout << hasil << endl ;
+
+    return 0;
+}
