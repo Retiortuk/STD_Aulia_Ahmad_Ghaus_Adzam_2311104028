251c8be298a9ff65eda5cf216247ec7051c3f386
RetiortUK <aauliagazzam@gmail.com>
2024-10-01 23:20:32 +0700

    feat: latihan modul 3

--- 02_Pengenalan_CPP_Bagian_2/UNGUIDED/LATIHAN_03.cpp
+++ 02_Pengenalan_CPP_Bagian_2/UNGUIDED/LATIHAN_03.cpp
@@ -9,22 +9,26 @@ int minimum(int array[], int len) {
             min = array[i];
         }
     }
-    return min;
-}
+    return min;}
 
-int maks(int array[], int len) {
+int maksimal(int array[], int len) {
     int maks = array[0];
     for (int i = 1; i < len; i++) {
         if (array[i] > maks) {
             maks = array[i];
         }
     }
-    return 0;
-}
+    return maks; }
+
+int rata(int array[], int len) {
+    int avg = array[0];
+    for (int i = 0; i < len; i++) {
+        avg += array[i];
+    }
+    return (int) avg / len;}
 
 int main(){
-
-    int len, pilihan;
+    int len, pilih;
 
     cout << "Masukkan Panjang Data Array: ";
     cin >> len;
@@ -35,7 +39,28 @@ int main(){
         cin >> array[i];
     }
 
+    do {
+        cout << "1. Cari Nilai Maksimum" << endl ;
+        cout << "2. Cari Nilai Minimal" << endl ;
+        cout << "3. Cari Nilai Rata-Rata" << endl ;
+        cout << "Pilih 1-3: ";
+        cin >> pilih;
 
+        switch (pilih)
+        {
+        case 1 :
+            cout << "Nilai Maksimum: " << maksimal(array, len) << endl ;
+            break;
+        case 2 :
+            cout << "Nilai Minimal: " << minimum(array, len) << endl ;
+            break;
+        case 3 :
+            cout << "Nilai Rata-Rata: " << rata(array, len) << endl ;
+            break;
+        default:
+            cout << "Program Berakhir" << endl ;
+        }
+    } while (pilih != 4);
 
     return 0;
 }
